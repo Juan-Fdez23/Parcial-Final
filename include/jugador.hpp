3c62fa27bb6870e6333709f512b02892d8dce9d6
Juan-Fdez23 <juandavidfernandezperez98@gmail.com>
2026-05-20 23:46:50 -0500

    Update jugador.hpp

--- include/jugador.hpp
+++ include/jugador.hpp
@@ -1,1 +1,11 @@
+#pragma once
+
+struct jugador{
+   int x, y;
+   int ItemCargado;
+   int hP;
+};
+
+struct EstadoDelJuego;
+void ActualizarPosicionDelJugador(EstadoDelJuego& juego, char tecla);
 
