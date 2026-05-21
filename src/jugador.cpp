28a4d7ec4c798b79cf1548aadb71225731275c1c
Juan-Fdez23 <juandavidfernandezperez98@gmail.com>
2026-05-21 00:22:29 -0500

    Update jugador.cpp

--- src/jugador.cpp
+++ src/jugador.cpp
@@ -1,1 +1,32 @@
+#include "../include/jugador.hpp"
+#include "../include/mapa.hpp"
 
+void ActualizarPosicionDelJugador(EstadoDelJuego juego, char tecla){
+    int nX = juego.player.x;
+    int nY = juego.player.y;
+
+    switch(tecla){
+        case 'w':{
+            nY--;
+            break;
+        };
+        case 's':{
+            nY++;
+            break;
+        };
+        case 'a':{
+            nX--;
+            break;
+        };
+        case 'd':{
+            nX++;
+            break;
+        }
+    }
+
+    if(!IsMuro(juego, nX, nY)){
+        juego.player.x = nX;
+        juego.player.y = nY;
+    }
+    TransicionDeHb(juego);
+}
