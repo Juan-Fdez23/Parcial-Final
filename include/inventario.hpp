c0d4ff94267a490a2b0454f89993ebecd251f09c
Juan-Fdez23 <juandavidfernandezperez98@gmail.com>
2026-05-20 23:35:50 -0500

    Update inventario.hpp

--- include/inventario.hpp
+++ include/inventario.hpp
@@ -1,1 +1,17 @@
+#pragma once
+
+struct inventario{
+    int x, y; 
+    int Sx, Sy; // Sean "Sx" & "Sy" donde se encuentran por primera vez los objetos.
+    int hb, Shb; // Sean "hb" &  "Shb" donde se encuentra el objeto (room).
+    int tipo;
+    bool IsActivo;
+};
+
+struct EstadoDelJuego;
+
+void IniciarObj(EstadoDelJuego& juego);
+void TomarObj(EstadoDelJuego& juego);
+void BotarObj(EstadoDelJuego& juego);
+void ReiniciarObj(EstadoDelJuego& juego);
 
