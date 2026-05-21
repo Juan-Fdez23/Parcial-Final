b24fc4177795bec0a3fbcc439b1518f7b9b4d776
Juan-Fdez23 <juandavidfernandezperez98@gmail.com>
2026-05-21 04:34:52 -0500

    Update inventario.cpp

--- src/inventario.cpp
+++ src/inventario.cpp
@@ -11,21 +11,31 @@ void IniciarObj(EstadoDelJuego& juego){
 
     juego.item[0].x = 10;
     juego.item[0].y = 5;
-    juego.item[0].hb = 0;
+    juego.item[0].hb = 2;
     juego.item[0].Sx = 10;
     juego.item[0].Sy = 5;
-    juego.item[0].Shb = 0;
+    juego.item[0].Shb = 2;
     juego.item[0].tipo = TipodeItem::Llave;
     juego.item[0].IsActivo = true;
 
-    juego.item[1].x = 15;
-    juego.item[1].y = 6;
+    juego.item[1].x = 1;
+    juego.item[1].y = 8;
     juego.item[1].hb = 1;
-    juego.item[1].Sx = 15;
-    juego.item[1].Sy = 6;
+    juego.item[1].Sx = 1;
+    juego.item[1].Sy = 8;
     juego.item[1].Shb = 1;
     juego.item[1].tipo = TipodeItem::Espada;
     juego.item[1].IsActivo = true;
+
+    juego.item[4].x = 1;
+    juego.item[4].y = 8;
+    juego.item[4].hb = 4;
+    juego.item[4].Sx = 1;
+    juego.item[4].Sy = 8;
+    juego.item[4].Shb = 4;
+    juego.item[4].tipo = TipodeItem::Espada;
+    juego.item[4].IsActivo = true;
+
 }
 
 void TomarObj(EstadoDelJuego& juego){
