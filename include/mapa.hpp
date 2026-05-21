b8d24f0981eb2804b68b36068db4e2a0d2abd16d
Juan-Fdez23 <juandavidfernandezperez98@gmail.com>
2026-05-21 00:15:48 -0500

    Update mapa.hpp

--- include/mapa.hpp
+++ include/mapa.hpp
@@ -1,1 +1,39 @@
+#pragma once
+
+#include "juego.hpp"
+#include "enemigo.hpp"
+
+const int c = 20; // Columnas
+const int r = 10; // Filas
+
+struct Salidas{
+    int THb;
+    int requerimiento;
+};
+
+struct R_Salidas{
+    static const int NA = 0;
+    static const int Llave = 1;
+    static const int Enemigo_Muerto = 2;
+};
+
+struct Hb{
+    char matriz[r][c];
+    Enemigo entidad1;
+
+    bool esEnemigo;
+    Salidas Norte;
+    Salidas Sur;
+    Salidas Este;
+    Salidas Oeste;
+};
+
+void inHb(EstadoDelJuego& juego);
+void DibujarMapa(EstadoDelJuego& juego);
+bool IsMuro(EstadoDelJuego& juego, int x, int y);
+bool PuedeSalir(EstadoDelJuego& juego, Salidas& salida);
+void TransicionDeHb(EstadoDelJuego& juego);
+
+void ActualizarEnemigo(EstadoDelJuego& juego);
+void AtacarEnemigo(EstadoDelJuego& juego);
 
