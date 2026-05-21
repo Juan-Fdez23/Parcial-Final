0082989cd92d6b5a6ebd4b3c69b37a1d1c8916a1
Juan-Fdez23 <juandavidfernandezperez98@gmail.com>
2026-05-21 00:15:25 -0500

    Update juego.hpp

--- include/juego.hpp
+++ include/juego.hpp
@@ -1,1 +1,28 @@
+#pragma once
 
+#include <iostream>
+
+#include "jugador.hpp"
+#include "inventario.hpp"
+#include "mapa.hpp"
+
+
+struct EstadoDelJuego{
+    jugador player;
+    inventario item[8];
+    Hb habitaciones[6];
+    int Hb_Actual;
+    int Velocidad_Enemigo;
+    bool JuegoEnLoop;
+};
+
+void Iniciar_Juego(EstadoDelJuego& juego);
+void Shutdown_Juego(EstadoDelJuego& juego);
+void Juego_Loop(EstadoDelJuego& juego);
+
+void ActualizarTerminal();
+void EsconderCursor();
+void MostrarCursor();
+void EnableUTF8();
+bool TeclaPresionada();
+char LeerTecla();
