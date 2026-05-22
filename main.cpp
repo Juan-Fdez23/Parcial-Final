a02e03bd72619f3b6ec036b34c92aa78623c2ccf
Juan-Fdez23 <juandavidfernandezperez98@gmail.com>
2026-05-22 12:52:55 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,15 @@
+#include "include/juego.hpp"
+#include "include/mapa.hpp"
 
+int main(){
+    EstadoDelJuego juego;
+
+    Iniciar_Juego(juego);
+
+    while(juego.JuegoEnLoop){
+        Juego_Loop(juego);
+    }
+
+    Shutdown_Juego(juego);
+    return 0;
+}
